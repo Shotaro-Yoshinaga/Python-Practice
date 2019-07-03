c96524edc263e0e69421424e78abdfe7ce83a393
Yoshinaga Shotaro <shotaro19963094@gmail.com>
2019-07-03 23:31:46 +0900

    add date2.py

--- date2.py
+++ date2.py
@@ -6,4 +6,14 @@ today = datetime.datetime.today()
 print(today)
 
 #明日の日付
-print(today + datetime.timedelta(days=1))
+print(today + datetime.timedelta(days=7))
+
+#2015年1月1日の一週間後
+newyear = datetime.timedelta(2015, 1, 1)
+print(newyear + datetime.timedelta(days=1))
+
+#2015年1月1日から今日までの日数
+calc = today - newyear
+
+#計算結果の戻り値は「timedelta」
+print(calc.days)
