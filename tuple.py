e1b474372eaa1e3cf09453daf0806232c4495f8b
Yoshinaga Shotaro <shotaro19963094@gmail.com>
2019-07-04 15:09:20 +0900

    edit tuple at qiita

--- tuple.py
+++ tuple.py
@@ -2,7 +2,8 @@ import datetime
 
 def get_today():
     today = datetime.datetime.today()
-    value = (today.year, today.month, today.day)
+    #カンマがあるとタプルになる
+    value = today.year, today.month, today.day
 
     return value
 
@@ -12,3 +13,7 @@ print(test_tuple)
 print(test_tuple[0])
 print(test_tuple[1])
 print(test_tuple[2])
+
+a = {}
+a[0, 0] = "ABC"
+print(a)
