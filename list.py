f0ff18726d9d550b21ba8c1bd84467f359545f74
Yoshinaga Shotaro <shotaro19963094@gmail.com>
2019-07-04 10:20:39 +0900

    edit list.py

--- list.py
+++ list.py
@@ -10,24 +10,26 @@ test_list_2 = []
 print(test_list_2)
 
 print('-------------------------------')
-
+#要素の追加
 test_list_2.append('python')
 test_list_2.append('web')
 test_list_2.append('aaaaa')
 test_list_2.append('zzzzz')
 print(test_list_2)
 
+print('-------------------------------')
+#インデックスを指定して追加
 test_list_1.insert(1,',')
 test_list_1.insert(3, '-')
 print(test_list_1)
 
 print('----------------------------------')
-
+#要素の削除_1
 test_list_1.remove('abc')
 print(test_list_1)
 
 print('----------------------------------')
-
+#要素の削除_2
 print(test_list_2.pop(3))
 print(test_list_2)
 
@@ -35,9 +37,9 @@ print(test_list_2.pop())
 print(test_list_2)
 
 print('----------------------------------')
-
+#要素のインデックスを取得
 print(test_list_1.index('efg'))
 
 print('----------------------------------')
-
+#リスト内での要素数の取得
 print(test_list_1.count('python'))
