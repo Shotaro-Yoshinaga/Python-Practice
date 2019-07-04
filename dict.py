e0936a3551f6529de4f43d1d8257557d37d5e847
Yoshinaga Shotaro <shotaro19963094@gmail.com>
2019-07-04 10:36:47 +0900

    edit dict.py

--- dict.py
+++ dict.py
@@ -18,3 +18,31 @@ print('----------------------------------')
 #valueの取得
 print(test_dict_1.get('YEAR', 'NOT FOUND'))
 print(test_dict_1.get('YEARS', 'NOT FOUND'))
+
+print('----------------------------------')
+#要素の追加
+test_dict_2 = {}
+test_dict_2['YEAR'] = '2017'
+test_dict_2['MONTH'] = '3'
+test_dict_2['DAY'] = '19'
+print(test_dict_2)
+
+print('----------------------------------')
+#要素の削除
+del test_dict_1['MONTH']
+print(test_dict_1)
+
+print('----------------------------------')
+#keyやvalueだけを取得する
+print(test_dict_2.keys())
+print(test_dict_2.values())
+
+print('----------------------------------')
+#keyとvalueの同時取得
+for key, value in test_dict_2.items():
+    print(key, value)
+
+print('----------------------------------')
+#keyを保持しているか確認
+print('YEAR' in test_dict_2)
+print('YEARS' in test_dict_2)
